6f67cb4fafa53615dac024c88df890ff3d6d2140
aytona <christopher.aytona@gmail.com>
2016-11-16 05:12:45 -0500

    Part 1 'completed'

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+
+    private Vector3 m_initPosition;
+    private Rigidbody m_rb;
+
+    private InitialImpulse m_initImpulse;
+    private LaunchImpulse m_launchImpulse;
+
+    private SoundManager m_soundManager;
+
+    void Start() {
+        m_initPosition = transform.position;
+        m_rb = GetComponent<Rigidbody>();
+
+        m_initImpulse = GetComponent<InitialImpulse>();
+        m_launchImpulse = GetComponent<LaunchImpulse>();
+
+        m_soundManager = FindObjectOfType<SoundManager>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "initTarget") {
+            Launch();
+        }
+        if (other.gameObject.tag == "launchTarget") {
+            StartCoroutine(LevelTransition());
+        }
+        if (other.gameObject.tag == "rampTarget") {
+            StartCoroutine(LevelFinish());
+        }
+    }
+
+    private void Launch() {
+        m_initImpulse.enabled = false;
+        m_rb.velocity = Vector3.zero;
+        m_launchImpulse.enabled = true;
+    }
+
+    private IEnumerator LevelTransition() {
+        m_soundManager.PlayLaunchSFX();
+        m_launchImpulse.enabled = false;
+        m_rb.velocity = Vector3.zero;
+        m_rb.isKinematic = true;
+        yield return new WaitForSeconds(m_soundManager.m_launchSFX.length + 1f);
+        transform.position = m_initPosition;
+        m_rb.isKinematic = false;
+
+    }
+
+    private IEnumerator LevelFinish() {
+        m_soundManager.PlayTargetSFX();
+        m_rb.velocity = Vector3.zero;
+        yield return new WaitForSeconds(m_soundManager.m_targetSFX.length + 1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip m_launchSFX;
+    public AudioClip m_targetSFX;
+
+    private AudioSource m_source;
+
+    void Start() {
+        m_source = GetComponent<AudioSource>();
+    }
+
+    public void PlayLaunchSFX() {
+        m_source.clip = m_launchSFX;
+        m_source.Play();
+    }
+
+    public void PlayTargetSFX() {
+        m_source.clip = m_targetSFX;
+        m_source.Play();
+    }
+}
